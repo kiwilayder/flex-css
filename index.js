1f205a828bed01fb4f247cc0f0c141e10846c33f
kiwilayder <kiwilay@mail.ru>
2022-11-28 19:22:06 +0400

    bug

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let popupBg = document.querySelector('.popup'); // Фон попап окна
 let popup = document.querySelector('.popup__form'); // Само окно
 let openPopupButtons = document.querySelectorAll('.menu__button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.popup__close'); // Кнопка для скрытия окна
+let closeSubmit = document.querySelector('.popup__submit');
 
 openPopupButtons.forEach((button) => { // Перебираем все кнопки
     button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
@@ -23,7 +24,7 @@ closePopupButton.addEventListener('click',() => { // Вешаем обработ
 });
 
 document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBg) { // Если цель клика - фот, то:
+    if(e.target === popupBg || e.target === closeSubmit ) { // Если цель клика - фот, то:
         popupBg.classList.remove('active'); // Убираем активный класс с фона
         popup.classList.remove('active'); // И с окна
     }
