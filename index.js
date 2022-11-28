e251b858f5511cc6e47fd6bd4c33845db16a5270
kiwilayder <kiwilay@mail.ru>
2022-11-28 19:10:39 +0400

    modal_frame

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector('.menu').classList.toggle('open');
     });
 });
+
+let popupBg = document.querySelector('.popup'); // Фон попап окна
+let popup = document.querySelector('.popup__form'); // Само окно
+let openPopupButtons = document.querySelectorAll('.menu__button'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.popup__close'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фот, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+    }
+});
